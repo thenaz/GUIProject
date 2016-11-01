0d82a1c2548634e0045b56a636bdda7e92897a31
thenaz <anaz7963@740s-pl-126.local>
2016-11-01 08:14:47 -0600

    Created JFrame and JPanel subclasses and made the contructors

---
+++ src/graphic/view/FirstFrame.java
@@ -0,0 +1,18 @@
+package graphic.view;
+
+import javax.swing.JFrame;
+
+import graphic.controller.FirstGUIController;
+
+public class FirstFrame extends JFrame
+{
+	private FirstGUIController baseController;
+	
+	public FirstFrame(FirstGUIController baseController)
+	{
+		super();
+		this.baseController = baseController;
+	}
+	
+	
+}
---
+++ src/graphic/view/FirstPane.java
@@ -0,0 +1,6 @@
+package graphic.view;
+
+public class FirstPane
+{
+
+}
