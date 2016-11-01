7ec60e22b4d9c3b580c656d1e6e023b8922a1f50
thenaz <anaz7963@740s-pl-126.local>
2016-11-01 08:58:43 -0600

    Made listeners and a lot of imports

--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -1,15 +1,48 @@
 package graphic.view;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import graphic.controller.FirstGUIController;
+import java.awt.Color;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
 	private FirstGUIController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(FirstGUIController baseController)
 	{
 		super();
 		this.baseController= baseController;
+		
+		colorButton = new JButton("Color!");
+		randomButton = new JButton("Random");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
 	}
+	private void setupPanel()
+	{
+		this.setBackground(Color.BLUE);
+		this.add(colorButton);
+		this.add(randomButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	
 }
