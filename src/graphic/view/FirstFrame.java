65d4f0e82698f793e3c985d2ff46b25897187cdf
thenaz <anaz7963@740s-pl-126.local>
2016-11-01 08:41:13 -0600

    Added this. in Frame

--- src/graphic/view/FirstFrame.java
+++ src/graphic/view/FirstFrame.java
@@ -7,17 +7,21 @@ import java.awt.Dimension;
 public class FirstFrame extends JFrame
 {
 	private FirstGUIController baseController;
+	private FirstPanel appPanel;
 	
 	public FirstFrame(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		this.appPanel = new FirstPanel(baseController);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("Wow a Window!!!");
 		this.setSize(new Dimension(500,500));
 		this.setVisible(true);
 	}
